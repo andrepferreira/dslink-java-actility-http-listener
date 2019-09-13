4b771f33e2154aacc6e9a5c303ade3bb2805fb13
Andre Ferreira <andre.ferreira@global.ntt>
2019-09-13 12:04:01 +0200

    Removed unused files

--- dslink_deploy_automation_part1 - this builds the solution.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Users\simon.ball\Downloads\OneDrive - Dimension Data\DSLink\template-dslink-java
-.\gradlew build distZip
--- dslink_deploy_automation_part2 - this deploys the solution.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Program Files\7-Zip
-7z x "C:\Users\simon.ball\Downloads\OneDrive - Dimension Data\DSLink\template-dslink-java\build\distributions\http-listener-dslink-0.0.1-SNAPSHOT.zip" -aoa -o"C:\Users\simon.ball\Downloads\OneDrive - Dimension Data\DSLink\template-dslink-java\build\distributions"
-Cd "C:\Users\simon.ball\Downloads\OneDrive - Dimension Data\DSLink\template-dslink-java\build\distributions\http-listener-dslink-0.0.1-SNAPSHOT"
-CMD /K ".\bin\http-listener-dslink -b https://10.12.126.55:8443/conn"
